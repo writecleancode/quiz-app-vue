c84938d5744eb48f05788e90b2aceb1c39a83d8e
Rafal <rafek131@wp.pl>
2025-01-08 16:13:05 +0100

    improvements to github pages error 404 handler function

--- src/utils/ghPagesReloadFix.ts
+++ src/utils/ghPagesReloadFix.ts
@@ -5,12 +5,14 @@ import { onMounted } from 'vue';
 export const handlePageReload = () => {
 	const router = useRouter();
 
-	onMounted(async () => {
+	onMounted(() => {
 		let path = localStorage.getItem('path');
 		if (path) {
-			await router.isReady();
 			localStorage.removeItem('path');
 			router.push(`/${path.slice(basePath.length)}`);
 		}
 	});
 };
+
+// fix created thanks to this tutorial:
+// https://medium.com/swlh/how-to-host-your-angular-reactjs-vuejs-spa-on-github-pages-2d9ab102ac7b
