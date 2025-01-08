6495f3f6e98129cb1d96336f1f2dfb63dfaa806d
Rafal <rafek131@wp.pl>
2025-01-08 12:37:40 +0100

    changes made to Vue Router for better routing experiance on github pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,3 +21,11 @@ export const router = createRouter({
 	history: createWebHashHistory(basePath),
 	routes,
 });
+
+router.beforeEach((to, from, next) => {
+	if (to.path === '/' && to.hash.startsWith('#/')) {
+		next(to.hash.substr(1));
+	} else {
+		next();
+	}
+});
